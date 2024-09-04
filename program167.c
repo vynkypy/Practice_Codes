ec0bb37e4f4b92f3901558ebd60f256b6a5ff69c
vynkypy <57390573+vynkypy@users.noreply.github.com>
2024-09-05 00:22:43 +0530

    Add files via upload

---
+++ program167.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int strlenCapX(char *str)
+{
+    int iCnt = 0;
+
+    while(*str != '\0')
+    {
+        if((*str >= 65) && (*str <= 90))
+        {
+            iCnt++;
+        }
+        str++;
+
+    }
+    return iCnt;
+}
+
+int main()
+{
+    char Arr[20];
+    int iRet = 0;
+
+    printf("Enter a String: \n");
+    scanf("%[^'\n']s", Arr);
+
+    iRet = strlenCapX(Arr);
+
+    printf("Number of capital  letters are : %d\n", iRet);
+
+    return 0;
+}
---
+++ program170.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int CountVowel(char *str)
+{
+    int iCnt = 0;
+
+    while(*str != '\0')
+    {
+        if((*str == 'A') || (*str == 'E')||(*str == 'I')||(*str == 'O')||(*str == 'U'))
+        {
+            iCnt++;
+        }
+        str++;
+
+    }
+    return iCnt;
+}
+
+int main()
+{
+    char Arr[20];
+    int iRet = 0;
+
+    printf("Enter a String: \n");
+    scanf("%[^'\n']s", Arr);
+
+    iRet = CountVowel(Arr);
+
+    printf("Number of capital  letters are : %d\n", iRet);
+
+    return 0;
+}
