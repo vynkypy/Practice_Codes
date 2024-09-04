b181f1155e8853195e5d77485a4a1c70b57922a0
vynkypy <57390573+vynkypy@users.noreply.github.com>
2024-09-04 09:19:47 +0530

    Add files via upload

---
+++ program145.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void Updator(int Arr[], int iSize)
+{
+    int i = 0;
+
+    for(i = 0; i < iSize; i++)
+    {
+        Arr[i] = Arr[i]*2;
+    }
+}
+
+int main()
+{
+    int *Brr = NULL;
+    int iCount = 0, i = 0, iRet = 0;
+
+    printf("Enter the number of elements that you want to enter: ");
+    scanf("%d", &iCount);
+
+    Brr =  (int *) malloc (iCount * sizeof(int));
+
+    printf("Enter the elements:\n");
+
+    for(i = 0; i< iCount; i++)
+    {
+        scanf("%d", &Brr[i]);
+    }
+
+    Updator(Brr, iCount);
+
+    printf("Data after updation is: ");
+
+    for(i = 0;  i< iCount; i++)
+    {
+        printf("%d\t", Brr[i]);
+    }
+
+    free(Brr);
+
+    return 0;
+}
